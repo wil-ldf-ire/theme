b068df71500a1234f1d2e2ec3c1c5dffa098222c
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-16 01:10:28 +0530

    getSlug instead of file _GET variable

--- assets/scss/init.php
+++ assets/scss/init.php
@@ -5,7 +5,7 @@ function error404() {
 	die();
 }
 
-if (!$_GET['file']) {
+if (!$dash->getSlug()) {
 	error404();
 }
 
@@ -13,9 +13,10 @@ $sass = new Sass();
 $sass->setStyle(Sass::STYLE_COMPRESSED);
 $sass->setEmbed(true);
 
-$sass_file = $_GET['file'] ?? null;
+$sass_file = ($dash->getSlug() ?? null);
 $sass_dir = THEME_PATH . '/assets/scss/';
 
+$css = '';
 /**
  * if requested file is "bootstrap" then load bootstrap.scss
  * otherwise load .scss file from "scss/"
@@ -33,6 +34,6 @@ if ($sass_file == 'bootstrap') {
 if ($css) {
 	header("Content-Type: text/css; charset=utf-8");
 	echo $css;
+} else {
+	error404();
 }
-
-error404();
