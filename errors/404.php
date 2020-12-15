7cd34c06f9a9d390814bed979588796a60360805
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-16 01:10:38 +0530

    404 file

--- errors/404.php
+++ errors/404.php
@@ -1,5 +1,7 @@
-<?php include_once (THEME_PATH.'/header.php'); ?>
-<div class="m-auto mt-0">
+<?php include_once __DIR__ . '/pages/_header.php'?>
+
+<div class="mx-auto mt-5">
   <h1 class="display-4">404 / Nothing found.</h1>
 </div>
-<?php include_once (THEME_PATH.'/footer.php'); ?>
+
+<?php include_once __DIR__ . '/pages/_footer.php'?>
