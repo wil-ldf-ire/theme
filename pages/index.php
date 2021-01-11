2a1986951fb2806fbca06040ac79601c517f788c
akshay / wildfire <akshay.wildfire@pm.me>
2021-01-11 19:07:14 +0530

    new bootstrap improvements

--- pages/index.php
+++ pages/index.php
@@ -1,25 +1,21 @@
 <?php include_once __DIR__ . '/_header.php'?>
 
-<div class="container p-4 mt-5">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="jumbotron">
-                <h1 class="text-center">
-                    We support our farmers in their fight against big corporations.
-                </h1>
+<main class="container">
+    <div class="starter-template text-center py-5 px-3">
+        <h1>
+            We support our farmers in their fight against big corporations.
+        </h1>
 
-                <div><img class="img-fluid" src="<?=THEME_URL;?>/assets/img/i-heart-kheti.jpg"></div>
+        <div><img class="img-fluid" src="<?=THEME_URL;?>/assets/img/i-heart-kheti.jpg"></div>
 
-                <p class="text-center mt-4">
-                    Found this during the protest at Singhu Border. Loved the fact that this was picked up from Milton Glaser's work and used so well.
-                </p>
+        <p class="mt-4">
+            Found this during the protest at Singhu Border. Loved the fact that this was picked up from Milton Glaser's work and used so well.
+        </p>
 
-                <p class="text-center mt-4 text-muted">
-                    A Conversation with <a href="https://creativecloud.adobe.com/cc/discover/article/a-conversation-with-milton-glaser?locale=en">Milton Glaser</a>.
-                </p>
-            </div>
-        </div>
+        <p class="mt-4 text-muted">
+            A Conversation with <a href="https://creativecloud.adobe.com/cc/discover/article/a-conversation-with-milton-glaser?locale=en">Milton Glaser</a>.
+        </p>
     </div>
-</div>
+</main>
 
 <?php include_once __DIR__ . '/_footer.php'?>
